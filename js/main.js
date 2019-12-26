db50942c9cd29252d61a887d4f883e696b733cd0
ea1nk <juan@ea1nk.com>
2019-12-26 18:28:13 +0100

    Added Elevation Graph

--- js/main.js
+++ js/main.js
@@ -256,16 +256,94 @@ function initializeChartCad(measure) {
   });
 }
 
+function initializeChartEle(measure) {
+  var canvas = document.getElementById("elevationGraph");
+  var ctx = canvas.getContext("2d");
+  
+
+  chartEle = new Chart(ctx, {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: "1",
+          borderColor: "rgb(204, 230, 255)",
+          borderWidth: 1,
+          //backgroundColor: 'rgb(204, 230, 255, 0.7)',
+          pointRadius: 0,
+          data: []
+        },
+        {
+          label: "2",
+          borderColor: "rgb(255, 238, 204)",
+          borderWidth: 1,
+          //backgroundColor: 'rgb(255, 238, 204, 0.7)',
+          pointRadius: 0,
+          data: []
+        }
+      ]
+    },
+    options: {
+      responsive: false,
+      maintainAspectRatio: true,
+      title: {
+        display: true,
+        text: measure,
+        position: "bottom",
+        padding: 5,
+        fontSize: 10,
+        fontColor: "#FFF",
+        fontStyle: "bold"
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: {
+              display: true,
+              color:"#666"
+            },
+            type: "time",
+            distribution: "linear",
+            time:{
+                format:'HH:mm',
+                unit:'minute',
+                stepSize:5
+            }
+          }
+        ],
+        yAxes: [
+          {
+            gridLines: {
+              display: true,
+              color:"#666"
+            }
+          }
+        ]
+      },
+      tooltips: {
+        mode: "index",
+        intersect: false
+      },
+      hover: {
+        mode: "index",
+        intersect: false
+      }
+    }
+  });
+}
+
 function printPower(data, series) {
   data = JSON.parse(data);
   var now;
   let powerData = [];
   let hrData = [];
   let cadData = [];
+  let eleData=[];
   let xlsData = [];
   let powerBuffer = [];
   let hrBuffer = [];
   let cadBuffer = [];
+  let eleBuffer = [];
   let dataSetTitle = data.file_id.manufacturer;
   let records = data.activity.sessions[0].laps[0].records;
   records.forEach(record => {
@@ -276,18 +354,24 @@ function printPower(data, series) {
         powerBuffer.push(record.power);
         hrBuffer.push(record.heart_rate);
         cadBuffer.push(record.cadence);
+        eleBuffer.push(record.altitude*1000);
       } else {
         var power = calcMed(powerBuffer);
         var hr = calcMed(hrBuffer);
         var cadence = calcMed(cadBuffer);
+        var elevation = calcMed(eleBuffer);
+
         powerData.push({ x: new Date(now.format()).getTime(), y: power });
         hrData.push({ x: new Date(now.format()).getTime(), y: hr });
         cadData.push({ x: new Date(now.format()).getTime(), y: cadence });
-        xlsData.push({tsmp:new Date(now.format()).getTime(), pwr:power, hr:hr, cad:cadence});
+        eleData.push({ x: new Date(now.format()).getTime(), y: elevation });
+
+        xlsData.push({tsmp:new Date(now.format()).getTime(), pwr:power, hr:hr, cad:cadence, elev:elevation});
 
         powerBuffer = [];
         hrBuffer = [];
         cadBuffer = [];
+        eleBuffer = [];
 
         now = moment(record.timestamp);
       }
@@ -299,21 +383,29 @@ function printPower(data, series) {
       chartPwr.data.datasets[0].label = dataSetTitle;
       chartHR.data.datasets[0].label = dataSetTitle;
       chartCad.data.datasets[0].label = dataSetTitle;
+      chartEle.data.datasets[0].label = dataSetTitle;
 
       chartPwr.data.datasets[0].data = powerData;
       chartHR.data.datasets[0].data = hrData;
       chartCad.data.datasets[0].data = cadData;
+      chartEle.data.datasets[0].data = eleData;
+
       xlsBuffer[0] = xlsData;
       jsonData[0] = data;
       $("#file1_adj").text(auxFileName);
     } else {
+
       chartPwr.data.datasets[1].label = dataSetTitle;
       chartHR.data.datasets[1].label = dataSetTitle;
       chartCad.data.datasets[1].label = dataSetTitle;
+      chartEle.data.datasets[1].label = dataSetTitle;
+
 
       chartPwr.data.datasets[1].data = powerData;
       chartHR.data.datasets[1].data = hrData;
       chartCad.data.datasets[1].data = cadData;
+      chartEle.data.datasets[1].data = eleData;
+
       xlsBuffer[1] = xlsData;
       jsonData[1] = data;
       $("#file2_adj").text(auxFileName);
@@ -323,7 +415,10 @@ function printPower(data, series) {
       chartPwr.data.datasets[series].data = powerData;
       chartHR.data.datasets[series].data = hrData;
       chartCad.data.datasets[series].data = cadData;
+      chartEle.data.datasets[series].data = eleData;
+
   }
+  
 
   chartPwr.update();
   chartPwr.render();
@@ -333,6 +428,9 @@ function printPower(data, series) {
 
   chartCad.update();
   chartCad.render();
+
+  chartEle.update();
+  chartEle.render();
 }
 
 function fillResume(data){
@@ -346,7 +444,7 @@ function fillResume(data){
     $(resume_table).find("span[id='fileName']").text(" File: " + auxFileName)  
     if(data.file_id.manufacturer) $(resume_table).find("span[id='device']").text(data.file_id.manufacturer)
     if(data.file_id.time_created)$(resume_table).find("span[id='date']").text(moment(data.file_id.time_created).format("YYYY-MM-DD HH:mm:ss"));
-    /*if(data.activity.sessions[0].total_distance) */$(resume_table).find("span[id='distance']").text(data.activity.sessions[0].total_distance + "Km")
+    /*if(data.activity.sessions[0].total_distance) */$(resume_table).find("span[id='distance']").text(data.activity.sessions[0].total_distance.toFixed(2) + "Km")
     var event_duration = moment.duration(data.activity.sessions[0].total_elapsed_time,'seconds');
     var elapsed_time = event_duration.format("HH:mm:ss");
     $(resume_table).find("span[id='elapsed']").text(elapsed_time)
@@ -364,7 +462,7 @@ function fillResume(data){
 
 /* EXPORT FUNCTIONS */
 function exportJPEG(id) {
-  var graphs = ["", "pwrGraph", "hrGraph", "cadenceGraph"];
+  var graphs = ["", "pwrGraph", "hrGraph", "cadenceGraph", "elevationGraph"];
   var canvas = document.getElementById(graphs[id]);
   var image = canvas.toDataURL("image/jpeg", 1.0);
   var blob = dataURItoBlob(image);
@@ -387,6 +485,8 @@ function adjustDataSeries(idx, val){
   var powerData = chartPwr.data.datasets[idx].data;
   var hrData = chartHR.data.datasets[idx].data;
   var cadData = chartCad.data.datasets[idx].data;
+  var eleData = chartEle.data.datasets[idx].data;
+
   calculated_offset = val - eval("file"+(idx+1)+"_offset"); 
   eval("file"+(idx+1)+"_offset = current_offset + calculated_offset")
   
@@ -405,7 +505,14 @@ function adjustDataSeries(idx, val){
     measure.x = measure.x + calculated_offset
   })
 
-  chartCad.data.datasets[idx].data = cadData  
+  chartCad.data.datasets[idx].data = cadData 
+  
+  eleData.forEach(measure =>{
+    measure.x = measure.x + calculated_offset
+  })
+
+  chartEle.data.datasets[idx].data = eleData 
+
 
   chartPwr.update();
   chartPwr.render();
@@ -413,6 +520,9 @@ function adjustDataSeries(idx, val){
   chartHR.render();
   chartCad.update();
   chartCad.render();
+  chartEle.update();
+  chartEle.render();
+
 }
 function exportJSON(id){
   var data = jsonData[id-1];
@@ -432,7 +542,8 @@ async function createXLS() {
           { header: 'TIMESTAMP', key: 'a', width: '20' },
           { header: 'POWER', key: 'b', width: '20' },
           { header: 'HEART RATE', key: 'c', width: '20' },
-          { header: 'CADENCE', key: 'd', width: '20' }
+          { header: 'CADENCE', key: 'd', width: '20' },
+          { header: 'ELEVATION', key: 'e', width: '20' }
       ]
       var data = createXLSData(xlsBuffer[i]);
       ws.addRows(data)
@@ -445,7 +556,7 @@ async function createXLS() {
 function createXLSData(data){
 var rows = [];
 data.forEach(entry =>{
-  rows.push({a:entry.tsmp, b:entry.pwr, c:entry.hr, d:entry.cad})
+  rows.push({a:entry.tsmp, b:entry.pwr, c:entry.hr, d:entry.cad, e:entry.elevation})
 })
 return rows;
 }
